7d801ceb49e6e12587d3e427f51270ca0a939037
Ermano Arruda <exa371@bham.ac.uk>
2018-05-08 20:14:38 +0100

    refactorin

--- send_UDP.py
+++ send_UDP.py
@@ -3,8 +3,9 @@ import time
 import daqmx
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-UDP_IP = "localhost"
+UDP_IP = "10.0.11.189"
 UDP_PORT = 50000
 
 print "UDP target IP:", UDP_IP
@@ -18,7 +19,7 @@ sock = socket.socket(socket.AF_INET, # Internet
 
 # Create a task object
 task = daqmx.tasks.Task()
-task.sample_rate = 900
+task.sample_rate = 300
 device = 'Dev2'
 channels = []
 
@@ -38,25 +39,29 @@ for channel_idx in range(MAX_CHANNELS):
 # task.setup_append_data()
 
 
-task.configure_sample_clock(sample_rate=900,samples_per_channel=1)
+task.configure_sample_clock(sample_rate=task.sample_rate,samples_per_channel=1)
 start = time.time()
 elapsed_time = 0
-sampleTime = 10 # 5 seconds
+sampleTime = 1 # 5 seconds
 sample_counter = 0
 quit = False
+
+print "Transmitting data... On a separate client, connect to UDP address: %s port: %d to receive it.\n\n"%(UDP_IP,UDP_PORT,)
 while not quit:
 	data, samples_per_channel_received = task.read()
-	sample_counter += 1
+	
 
-	print "Sample DATA: ", str(data[0])[1:-1]
+	#print "Sample DATA: ", str(data[0])[1:-1]
 	sdata = str(data[0])[1:-1] + '\n'
 	sock.sendto(sdata, (UDP_IP, UDP_PORT))
-
+	sample_counter += 1	
 
 	elapsed_time = time.time() - start
 	if elapsed_time > sampleTime:
-		quit = True
+		sys.stdout.write("Current Loop Frequency: %d Hz\r"%(sample_counter,))
+		sample_counter = 0
+		start = time.time()
 
-		print "Time is up!"
+		# print "Time is up!"
 
 
